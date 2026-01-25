339fe8ac561f4b2d4fcd7ca0ecdfafe5ec77da0f
Bablu <krishnabablu22@gmail.com>
2026-01-25 23:22:23 +0530

    real jwt login added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,42 @@
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-  res.json({
-    user: { email },
-    token: "dummy-token",
-  });
+// LOGIN
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. user check
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // 2. password check
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // 3. generate JWT
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    // 4. response
+    res.json({
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+      token,
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 };
