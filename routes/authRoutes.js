f4c73dac49e2ea189854b76849fcaa6d4ccd7a68
Bablu <krishnabablu22@gmail.com>
2026-02-02 22:30:01 +0530

    conflict fixed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
 const {
   register,
-  login
+  login,
+  profile
 } = require("../controllers/authController");
-
-// REGISTER
-router.post("/register", register);
-
-// LOGIN
-router.post("/login", login);
-
-module.exports = router;
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/profile", authMiddleware, (req, res) => {
-  res.json({
-    message: "Protected route accessed",
-    userId: req.userId
-  });
-});
+router.post("/register", register);
+router.post("/login", login);
+router.get("/profile", authMiddleware, profile);
+
+module.exports = router;
